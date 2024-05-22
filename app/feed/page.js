8258b8d07522eb47fc035cee3deb023d5ab6e0f0
UzairKhan313 <uzairkhaan2003@gmail.com>
2024-05-22 21:32:31 +0500

    Working with optimizing application

--- app/feed/page.js
+++ app/feed/page.js
@@ -1,5 +1,15 @@
-import Posts from '@/components/posts';
-import { getPosts } from '@/lib/posts';
+import Posts from "@/components/posts";
+import { getPosts } from "@/lib/posts";
+
+// Working with dynamic metadata.
+export async function generateMetadata(data) {
+  const posts = await getPosts();
+  const numberOfPost = posts.length;
+  return {
+    title: `Browse all our ${numberOfPost} Posts`,
+    description: "Browse all our Posts",
+  };
+}
 
 export default async function FeedPage() {
   const posts = await getPosts();
